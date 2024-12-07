a4755917d3584a699746af7bb4e3b67e5fa0ab46
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2024-12-07 21:12:46 +0530

    Initials commits

--- app/src/main/java/com/example/controlgame/Constant.kt
+++ app/src/main/java/com/example/controlgame/Constant.kt
@@ -8,8 +8,6 @@ import java.math.BigInteger
 
 val web3j: Web3j = Web3j.build(HttpService("https://sepolia.infura.io/v3/8c09f9f92fc4438fbbce47b4a3eb2b3d")) // Replace with Infura/Alchemy node URL
 
-//val credentials = Credentials.create("0x0b3b687978b28cf6ddf2bd395850767fb1c1e821123ff541c6e278b5c1a857f1")
-// Gas provider for transactions
 val gasPrice = BigInteger.valueOf(20_000_000_000L) // 20 Gwei
 val gasLimit = BigInteger.valueOf(500_000)         // Gas limit
 val gasProvider = StaticGasProvider(gasPrice, gasLimit)
